726053b5ab79e33305ddb931dfa73c2b5d5b33cd
David_TC <davidcoraza0316@gmail.com>
2021-10-21 13:58:21 -0500

    Alerts for empty fields working

--- js/script.js
+++ js/script.js
@@ -13,17 +13,35 @@ const reset = () => {
 const encrypt = (e) =>{
     const encrypt_text = document.getElementById("encrypt_text").value;
     const shift_encrypt = document.getElementById("shift_encrypt").value;
-    const result = cesar(encrypt_text, shift_encrypt);
-    document.getElementById('encrypt_encrypt').value = result;
-    document.getElementById("shift_decrypt").value = shift_encrypt;
-    document.getElementById("decrypt_encrypt").value = result;
+    if( encrypt_text && shift_encrypt ){
+        const result = cesar(encrypt_text, shift_encrypt);
+        document.getElementById('encrypt_encrypt').value = result;
+        document.getElementById("shift_decrypt").value = shift_encrypt;
+        document.getElementById("decrypt_encrypt").value = result;
+    }else{
+        Swal.fire({
+            title: 'Atención',
+            text: 'Hay campos vacíos',
+            icon: 'error',
+            confirmButtonText: 'Aceptar'
+        })
+    }
 }
 
 const decrypt = (e) =>{
     const decrypt_text = document.getElementById("decrypt_encrypt").value;
     const shift_decrypt = document.getElementById("shift_decrypt").value;
-    const result = cesar(decrypt_text, (shift_decrypt*-1));
-    document.getElementById('decrypt_text').value = result;
+    if( decrypt_text && shift_decrypt ){
+        const result = cesar(decrypt_text, (shift_decrypt*-1));
+        document.getElementById('decrypt_text').value = result;
+    }else{
+        Swal.fire({
+            title: 'Atención',
+            text: 'Hay campos vacíos',
+            icon: 'error',
+            confirmButtonText: 'Aceptar'
+        });
+    }
 }
 
 const cesar = (text, shift) => {
